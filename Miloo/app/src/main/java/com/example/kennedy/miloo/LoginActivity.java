5726ae4761850f912ff7a6efb4f150363061eb58
Felipe Porfirio <felrpo@gmail.com>
2017-05-16 21:46:48 -0300

    Layout Login
    Iniciada a criação do layout de login com o Kennedy

--- Miloo/app/src/main/java/com/example/kennedy/miloo/LoginActivity.java
+++ Miloo/app/src/main/java/com/example/kennedy/miloo/LoginActivity.java
@@ -22,6 +22,7 @@ import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -65,16 +66,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Esconde a barra de titulos
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         // Set up the login form.
+
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
+
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
+
                     attemptLogin();
                     return true;
                 }
