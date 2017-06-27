92d907ca8a70ac9aec9ce7ca06d8a3bb90265afb
Unknown <felrpo@gmail.com>
2017-06-27 20:57:40 -0300

    Layout Filtros

--- Miloo/app/src/main/java/com/example/kennedy/miloo/FiltrosActivity.java
+++ Miloo/app/src/main/java/com/example/kennedy/miloo/FiltrosActivity.java
@@ -2,6 +2,8 @@ package com.example.kennedy.miloo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class FiltrosActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class FiltrosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+    }
+
+
+    public void onToggleClickedMaisRelevantes ( View view)
+    {
+        boolean checked = ((ToggleButton)view).isChecked();
+        if(checked)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
